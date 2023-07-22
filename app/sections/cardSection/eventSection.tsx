81a9e63f3f9c4f53997628a4ca66492f7c170348
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-07-22 12:22:25 +0530

    Auto scroll last slide bug fix

--- app/sections/cardSection/eventSection.tsx
+++ app/sections/cardSection/eventSection.tsx
@@ -22,7 +22,50 @@ const EventSection = () => {
         transform: translate3d(0, 0, 0);
     }
 `;
+    const [i, setI] = useState(1);
 
+    let l = events.length;
+    const ReduceOne = () => {
+        if (i === 1)
+            setI(l - 2);
+        else
+            setI(i - 1);
+    }
+    const AddOne = () => {
+        const screenWidth = globalThis.window?.innerWidth;
+
+        if (screenWidth > 1500) {
+            if (i === l - 1 && scrolling === 1) {
+                setI(1);
+            } else if (scrolling === 1) {
+                setI(i + 1);
+            }
+        } else if (screenWidth >= 800 && screenWidth <= 1500) {
+            if (i === l - 2 && scrolling === 1) {
+                setI(1);
+            } else if (scrolling === 1) {
+                setI(i + 1);
+            }
+        } else {
+            // For screens less than 800px, keep the existing behavior
+            if (i === l && scrolling === 1) {
+                setI(1);
+            } else if (scrolling === 1) {
+                setI(i + 1);
+            }
+        }
+    }
+
+    const [seconds, setSeconds] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+            AddOne();
+        }, 3000);
+        return () => { clearInterval(interval) };
+    }, [seconds]);
+
+    const [scrolling, setScrolling] = useState(1);
     return (
         <div className="card-row3">
             <div className='row3-card1' style={globalThis.window?.innerWidth > 1500 ? { width: '600px' } : { width: '100%' }}>
@@ -61,13 +104,13 @@ const EventSection = () => {
                     </a>
                 </div>
                 <div className='event-carousel flex gap-4 mt-2 overflow-hidden' style={globalThis.window?.innerWidth > 1500 ? { maxWidth: `${globalThis.window?.innerWidth - 700}px` } : { minWidth: '100%' }}
-
+                    onMouseEnter={() => { setScrolling(0) }} onMouseLeave={() => { setScrolling(1) }}
                 >
 
                     {event.map((index) => {
                         return (
                             <div className='border border-gray-200 h-[300px] min-w-[300px] flex flex-col rounded-lg animate'
-                                style={{ transform: `translate(${(-316 * (9 - 1))}px,0px)` }}
+                                style={{ transform: `translate(${(-316 * (i - 1))}px,0px)` }}
                             >
                                 <div className='h-[full] w-full flex-[4] round-top hover-effect' style={{ backgroundImage: `url(${index.url})`, backgroundSize: '100% 100%' }}>
 
