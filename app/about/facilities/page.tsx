36e0aa88186b7e9200ba5fd3ce4934dbfcbde9bb
Anmol376OP <anmolsrivastav7102@gmail.com>
2023-07-27 02:02:46 +0530

    facility

--- app/about/facilities/page.tsx
+++ app/about/facilities/page.tsx
@@ -1,20 +1,34 @@
 'use client';
 import NavBar from '../../sections/appbar/secondNav';
 import Footer from '../../sections/footer/footer';
-import Comingsoon from '../../designSystem/comingsoon';
+import Carousel from './carousel';
 import { useEffect } from 'react';
+import Data from './facilityData'
+import Card from './facilityCard'
 
 const App: React.FC = () => {
+    const carouselData = [{
+        index: 0,
+        url: 'https://3.img-dpreview.com/files/p/E~TS590x0~articles/8692662059/8283897908.jpeg'
+    },
+    {
+        index: 1,
+        url: 'https://i.pinimg.com/originals/8b/8d/de/8b8dde74631a704a0420ede695fde00d.png'
+    },
+    {
+        index: 2,
+        url: 'https://fujifilm-x.com/products-cameras-static/gfx100s/assets/images/image-setting/banner_wapk.png'
+    }
+    ]
     useEffect(() => {
         document.title = 'Facilities | Indian Institute of Information Technology, Kalyani';
     }, []);
     return (
         <>
-
             <NavBar />
-            <Comingsoon />
+            <Carousel carouselData={carouselData} />
+            <Card heading='Campus' desc='This' />
             <Footer />
-
         </>
     );
 };
