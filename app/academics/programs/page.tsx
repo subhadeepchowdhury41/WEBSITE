a1bbb3e0d8827ecfb7c15cf0c5e5f1e9c43f2166
Abhishree <abhishree881@gmail.com>
2023-08-02 17:52:42 +0530

    modified

--- app/academics/programs/page.tsx
+++ app/academics/programs/page.tsx
@@ -42,7 +42,7 @@ function ProgramPage() {
                     {matchingItem?.resources?.map((index) => {
                         return (
                             // <a href={index.url} target="_blank" rel="noopener noreferrer">
-                            <div onClick={() => handleButtonClick("AnnexureE.pdf")} className='border border-black p-[4px] rounded-[5px]'>{index.heading}</div>
+                            <div style={{ cursor: "pointer" }} onClick={() => handleButtonClick("AnnexureE.pdf")} className='border border-black p-[4px] rounded-[5px]'>{index.heading}</div>
                             // </a>
                         )
                     })}
