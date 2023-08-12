415b97f95936a062f2991a83622e1e9ca07aa4c9
abhi <abhishree881@gmail.com>
2023-08-12 23:02:12 +0530

    modified addmission page

--- app/about/staff/page.tsx
+++ app/about/staff/page.tsx
@@ -55,7 +55,22 @@ const Staff = () => {
     MapDP.set("Sourav", "/staff/sourav.webp");
     MapDP.set("Debottam", "/staff/db.webp");
 
+
+
+    const [scrollToSectionId, setScrollToSectionId] = useState<string | null>(null);
+    useEffect(() => {
+        if (scrollToSectionId) {
+            scrollToSection(scrollToSectionId);
+        }
+    }, [scrollToSectionId]);
+
+    const scrollToSection = (sectionId: string) => {
+        document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
+        setScrollToSectionId(null);
+    };
+
     return (
+
         <>
             <div className="navbar">
                 <NavBar />
@@ -68,10 +83,15 @@ const Staff = () => {
                         <h1>IIIT Kalyani Technical Team</h1>
                     </span>
                     <hr />
-                    <div className={isDesktop ? "staff-cards" : "small-staff-cards"}>
+                    {/* <div className={isDesktop ? "staff-cards" : "small-staff-cards"}>
                         {isDesktop ? <StaffCard name='Md. Samim Rahaman' position='Senior Technical Assistant' qualification='MCA in Computer Science' email='samim AT iiitkalyani.ac.in' subjects={['Computer Hardware and Networking', 'Python', 'DBMS', 'Data Structure', 'Algorithm-II', 'Operating System', 'Design and analysis of Algorithm-I', 'Compiler Design']} site='' dp={MapDP.get('Samim')} /> : <SmallStaffCard name='Md. Samim Rahaman' position='Senior Technical Assistant' qualification='MCA in Computer Science' email='samim AT iiitkalyani.ac.in' subjects={['Computer Hardware and Networking', 'Python', 'DBMS', 'Data Structure', 'Operating System', 'Design and analysis of Algorithm-I', 'Algorithm-II', 'Compiler Design']} site='' dp={MapDP.get('Samim')} />}
                         {isDesktop ? <StaffCard name='Amit Kumar Sadhukhan' position='Technical Assistant' qualification='B.Tech in Electronics and Communication Engineering' email='aks1991 AT yahoo.com' subjects={['Electronics', 'Microprocessor and microcontroller', 'Compiler Design', 'Programming with C/C++', 'Data Structure', 'Java', 'Scilab']} site='' dp={MapDP.get("Amit")} /> : <SmallStaffCard name='Amit Kumar Sadhukhan' position='Technical Assistant' qualification='B.Tech in Electronics and Communication Engineering' email='aks1991 AT yahoo.com' subjects={['Electronics', 'Microprocessor and microcontroller', 'Compiler Design', 'Java', 'Scilab', 'Programming with C/C++', 'Data Structure']} site='' dp={MapDP.get("Amit")} />}
                         {isDesktop ? <StaffCard name='Sreejoyee Das' position='Technical Assistant' qualification='B.Tech in Computer Science and Engineering' email='sreejoyee AT iiitkalyani.ac.in' subjects={['Data Structure', 'Java', 'Scilab', 'Programming with C/C++', 'Operating systems', 'Design and analysis of Algorithm-I', 'Algorithm-II']} site='https://sites.google.com/view/sreejoyee/home' dp={MapDP.get('Sreejoyee')} /> : <SmallStaffCard name='Sreejoyee Das' position='Technical Assistant' qualification='B.Tech in Computer Science and Engineering' email='sreejoyee AT iiitkalyani.ac.in' subjects={['Data Structure', 'Java', 'Scilab', 'Programming with C/C++', 'Operating systems', 'Design and analysis of Algorithm-I', 'Algorithm-II']} site='https://sites.google.com/view/sreejoyee/home' dp={MapDP.get('Sreejoyee')} />}
+                    </div> */}
+                    <div className="employee-cards">
+                        <EmployeeCard name='Md. Samim Rahaman' position='Senior Technical Assistant' email='amim AT iiitkalyani.ac.in' dp={MapDP.get("Samim")} />
+                        <EmployeeCard name='Amit Kumar Sadhukhan' position='Technical Assistant' email='aks1991 AT yahoo.com' dp={MapDP.get("Amit")} />
+                        <EmployeeCard name='Sreejoyee Das' position='Technical Attendant' email='sreejoyee AT iiitkalyani.ac.in' dp={MapDP.get("Sreejoyee")} />
                     </div>
                     <span className='staff-sub-header'>
                         <h1>IIIT Kalyani Employees</h1>
